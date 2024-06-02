1c2adfc405fc7fc682cc4f5cb8eeaca3a7989bd8
Roland <rolandthee4th@gmail.com>
2024-06-02 14:48:25 -0400

    Finished PostProcessingManager.cs
    Updated and Finished The Post Processing manager.

--- Assets/-Scripts/PostProcessingManager.cs
+++ Assets/-Scripts/PostProcessingManager.cs
@@ -1,18 +1,41 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
-public class PostProcessingManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public class PostProcessingManager : MonoBehaviour{
+    private static PostProcessingManager instance;
+    [Header("Settings")] 
+    [SerializeField] private float maxFog;
+    [SerializeField] private float minFog;
+    [Header("References")]
+    [SerializeField] private Volume deathVolume;
+    [SerializeField] private Volume normalVolume;
+    
+    private float lerpToWeight;
+    private float weight;
+    private float time;
+    private void Awake()
     {
+        if (instance != null)
+            Destroy(instance.gameObject);
+        instance = this;
+    }
+    private void Update()
+    {
+        var weightPerSec = (weight - lerpToWeight) / time;
+        weight -= weightPerSec * Time.deltaTime;
         
+        weight = Mathf.Clamp(weight, 0, 1);
+        deathVolume.weight = weight;
+        instance.normalVolume.weight = 1 - weight;
+
+        RenderSettings.fogDensity = (instance.maxFog - instance.minFog) * weight + instance.minFog;
     }
 
-    // Update is called once per frame
-    void Update()
+    public static void SetDeathWeight(float weight, float time)
     {
-        
+        instance.lerpToWeight = Mathf.Clamp(weight, 0, 1);
+        instance.time = time;
     }
 }
